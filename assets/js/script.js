95c628759c6d1cb1fa4ec05758d773a34eb25278
Kawser Ahmed Roni <kawserahmedroni51@gamil.com>
2024-12-10 15:50:29 +0600

    comited

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+/*-----------------------------------------------------------------
+
+Template Name:  Foresty - Ecology and Environment Template
+Author:  Gramentheme
+Author URI: https://themeforest.net/user/gramentheme/portfolio
+Developer: Kawser Ahmed Roni
+Version: 1.0.0
+Description: Foresty - Ecology and Environment Template
+
+-------------------------------------------------------------------
+CSS TABLE OF CONTENTS
+-------------------------------------------------------------------
+
+01. preloader
+02. header
+03. swiper slider
+04. animated text with swiper slider
+05. shop products count
+06. image src change
+07. hide & show a div
+08. isotope
+09. add class & remove class
+10. magnificPopup
+11. back to top
+12. data backgrund
+13. coundown by click
+14. remove products
+15. wow animation
+
+------------------------------------------------------------------*/
+
+(function ($) {
+	("use strict");
+})(jQuery);
